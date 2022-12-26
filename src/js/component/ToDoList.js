447a17a7e1ec00d0ce84043f3c18d03f25af3eae
Galdea <gabriel.aldea@gmail.com>
2022-12-26 19:56:30 +0000

    fix icon to right side missing

---
+++ src/js/component/ToDoList.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Column, Row, Container} from "react-bootstrap"
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [hover, setHover] = useState(-1);
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleTaskDelete = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  };
+
+  return (
+    <Container>
+      <Row>
+        <div className="title mt-5">
+          <p>todos</p>
+        </div>
+      </Row>
+      <Row className="box mx-5 p-5">
+
+      <Row className="justify-content-center">
+        <input
+          type="text"
+          className="input"
+          value={newTask}
+          onChange={(event) => setNewTask(event.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+      </Row>
+      <Row>
+        {tasks.length === 0 ? (
+          <p>No tasks, add a task</p>
+        ) : (
+          <ul>
+            {tasks.map((task, index) => (
+              <li
+                className="items d-flex justify-content-start px-3 py-1"
+                key={index}
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(-1)}
+              >
+                {task}
+                {hover === index && (
+                  <span onClick={() => handleTaskDelete(index)}>
+                    <i className="icon fa fa-times px-5" />
+                  </span>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+      </Row>
+      <Row>
+        <div className="todosLength">{tasks.length} items left</div>
+      </Row>
+      </Row>
+    </Container>
+  );
+}
+
+export default ToDoList;
