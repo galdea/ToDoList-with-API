8c0d9f797611445ba96e8ffea77aff34f8e738ba
Galdea <gabriel.aldea@gmail.com>
2022-12-30 20:47:53 +0000

    todo list with api fetch

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Column, Row, Container} from "react-bootstrap"
 
 
@@ -7,6 +7,16 @@ function ToDoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
   const [hover, setHover] = useState(-1);
+  const [input, setInput] = useState("");
+	const [lista, setLista] = useState([]);
+
+	useEffect(() => {});
+	useEffect(() => {
+		console.log("Renderizing");
+		getTodo().then(lista => {
+			setLista(lista.map(dato => dato.label));
+		});
+	}, []);
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
@@ -19,49 +29,169 @@ function ToDoList() {
     setTasks(tasks.filter((task, i) => i !== index));
   };
 
+const createTodo = async () => {
+	try {
+		return await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/galdea",
+			{
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify([]),
+				redirect: "follow"
+			}
+		).then(response => response.json());
+	} catch (error) {
+		console.log(error);
+		return { result: "Error al actualizar" };
+	}
+};
+const getTodo = async () => {
+	try {
+		return await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/galdea",
+			{
+				method: "GET",
+				redirect: "follow"
+			}
+		).then(response => response.json());
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
+};
+const putTodo = async (data = []) => {
+	try {
+		return await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/galdea",
+			{
+				method: "PUT",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(data),
+				redirect: "follow"
+			}
+		).then(response => response.json());
+	} catch (error) {
+		console.log(error);
+		return { result: "Error al actualizar" };
+	}
+};
+const deleteTodo = async () => {
+	try {
+		return await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/galdea",
+			{
+				method: "DELETE",
+				redirect: "follow"
+			}
+		).then(response => response.json());
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
+};
+
   return (
     <Container>
       <Row>
-        <div className="title mt-5">
+        <div className="title mt-5 d-flex justify-content-center">
           <p>todos</p>
         </div>
       </Row>
       <Row className="box mx-5 p-5">
 
       <Row className="justify-content-center">
-        <input
-          type="text"
+      <input
+					type="text"
           className="input"
-          value={newTask}
-          onChange={(event) => setNewTask(event.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-      </Row>
-      <Row>
-        {tasks.length === 0 ? (
-          <p>No tasks, add a task</p>
-        ) : (
-          <ul>
-            {tasks.map((task, index) => (
-              <li
-                className="items d-flex justify-content-start px-3 py-1"
-                key={index}
-                onMouseEnter={() => setHover(index)}
-                onMouseLeave={() => setHover(-1)}
-              >
-                {task}
-                {hover === index && (
-                  <span onClick={() => handleTaskDelete(index)}>
-                    <i className="icon fa fa-times px-5" />
-                  </span>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
-      </Row>
-      <Row>
-        <div className="todosLength">{tasks.length} items left</div>
+					placeholder="write your tasks..."
+					onChange={e => {
+						setInput(e.target.value);
+						console.log(input);
+					}}
+					value={input}
+					onKeyUp={e => {
+						if (e.keyCode === 13) {
+							if (input !== "") {
+								if (lista.length == 0) {
+									createTodo().then(val => {
+										let dato = [];
+										[...lista, input].forEach(value => {
+											dato.push({
+												label: value,
+												done: false
+											});
+										});
+										putTodo(dato).then(val => {
+											getTodo().then(lista => {
+												setLista(
+													lista.map(
+														dato => dato.label
+													)
+												);
+											});
+										});
+										setInput("");
+									});
+								} else {
+									let dato = [];
+									[...lista, input].forEach(value => {
+										dato.push({
+											label: value,
+											done: false
+										});
+									});
+									putTodo(dato).then(val => {
+										getTodo().then(lista => {
+											setLista(
+												lista.map(dato => dato.label)
+											);
+										});
+									});
+									setInput("");
+								}
+							}
+						}
+					}}
+				/>
+        <ul>
+					{lista.map((valorlista, key) => {
+						return (
+							<li className="items px-3 py-1" key={key}>
+								{valorlista}
+								<a
+									onClick={() => {
+										lista.splice(key, 1);
+										//setLista([...lista]);
+										let dato = [];
+										[...lista].forEach(value => {
+											dato.push({
+												label: value,
+												done: false
+											});
+										});
+										if (dato.length > 0) {
+											putTodo(dato).then(val => {
+												getTodo().then(lista => {
+													setLista(
+														lista.map(
+															dato => dato.label
+														)
+													);
+												});
+											});
+										} else {
+											deleteTodo().then(value => {
+												setLista([]);
+											});
+										}
+									}}>
+									                    <i className="icon fa fa-times py-1" />
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+        <div className="todosLength d-flex justify-content-center">{lista.length} items left</div>
       </Row>
       </Row>
     </Container>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,94 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ToDoList from "./ToDoList";
-import { useEffect, useState } from 'react';
 
 const Home = () => {
-
-  const createTodoList = () => {
-    const [status, setStatus] = useState(null);
-
-    useEffect(() => {
-      const createList = async () => {
-        try {
-          const url = "https://assets.breatheco.de/apis/fake/todos/user/galdea";
-          const data = {
-            "todos": [
-              { "label": "Make the bed", "done": false },
-              { "label": "Walk the dog", "done": false },
-              { "label": "Do the replits", "done": false }
-            ]
-          };
-
-          const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-          });
-
-          if (response.ok) {
-            setStatus('success');
-          } else {
-            setStatus('error');
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      }
-
-      createList();
-    }, []);
-
-    return status;
-  }
-
-  const updateTodoList = () => {
-    const [status, setStatus] = useState(null);
-
-    useEffect(() => {
-      const updateList = async () => {
-        try {
-          const url = "https://assets.breatheco.de/apis/fake/todos/user/galdea";
-          const data = {
-            "todos": [
-              { "label": "Make the bed", "done": true },
-              { "label": "Walk the dog", "done": false },
-              { "label": "Do the replits", "done": false }
-            ]
-          };
-
-          const response = await fetch(url, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-          });
-
-          if (response.ok) {
-            setStatus('success');
-          } else {
-            setStatus('error');
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      }
-
-      updateList();
-    }, []);
-
-    return status;
-  }
-  
-  return (
-    <div className="text-center">
-      <ToDoList />
-      {createTodoList()}
-      {updateTodoList()}
-    </div>
-  );
-};
-
+	return (
+		<div className="bg">
+			<ToDoList />
+		</div>
+	)
+	;
+}
 export default Home;
